e22ec870856e98dd3b20810c272255c4960ffc5a
LeeSinLiang <mangojamlee@gmail.com>
2025-10-12 16:13:30 -0400

    fix: removed test env

--- test_environment/sample_code.py
+++
@@ -1,9 +0,0 @@
-def hello_world():
-    print("Hello, world!")
-
-class Greeter:
-    def __init__(self, name):
-        self.name = name
-
-    def greet(self):
-        print(f"Hello, {self.name}!")
